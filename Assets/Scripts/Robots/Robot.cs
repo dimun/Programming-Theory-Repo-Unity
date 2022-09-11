ce3c2f57d2a6516a05229fb1a2498a979abe830a
Diego Fernando Muñoz C <dgmzcrrls@gmail.com>
2022-09-11 10:52:03 +0300

    basic controls added

--- Assets/Scripts/Robots/Robot.cs
+++ Assets/Scripts/Robots/Robot.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Robot : MonoBehaviour
 {
+    private Rigidbody robotRb;
+    public float rotationSpeed = 4.0f;
+    public float speed = 13.0f;
+
+    private void Start()
+    {
+        robotRb = GetComponent<Rigidbody>();
+    }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Move();
-        Clean();
     }
 
     public virtual void Move()
     {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
 
+        robotRb.AddForce(transform.forward * verticalInput * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        transform.Rotate(0, rotationSpeed * horizontalInput, 0);
     }
 
     public virtual void Clean()
